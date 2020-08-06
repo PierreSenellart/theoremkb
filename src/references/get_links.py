4206efdb10ad76492d78b8ce75c481d838435cb4
Theo Delemazure <tdelemazure@di.ens.fr>
2020-08-06 19:46:33 +0200

    update links obtention

--- src/references/get_links.py
+++ src/references/get_links.py
@@ -7,9 +7,11 @@ import re
 from TexSoup import TexSoup
 
 
-from ..config import SRC_PATH, STUFF_PATH
+from ..config import SRC_PATH, STUFF_PATH, GRAPH_PATH
 
-path_pdf = SRC_PATH + "/src/"
+path_pdf = SRC_PATH + "/pdf/"
+path_links = STUFF_PATH + "/links.csv"
+
 
 grobid_url = 'http://localhost:8070/api/'
 grobid_references = grobid_url + 'processReferences'
@@ -54,15 +56,15 @@ def pdf2xml(pdf_path,kind='references'):
 # of other references to find the grobid number of each reference
 
 def getitem(pdf_path,title_list,dict_output):
-	(out_code,xmlTree) = pdf2xml(pdf_path,'references')
-	title_numb = []
-
+	(out_code,xmlTree) = pdf2xml(pdf_path,'fulltext')
+	title_numb = {}
+	idx2idf = {}
 	title_list_copy = title_list[::]
 	if out_code != 200:
 		for title,ref in title_list_copy:
-			title_numb.append((ref,-1))
+			title_numb.append[ref] = -1
 			dict_output["GROBID_ERR"] += 1
-		return title_numb
+		return title_numb,idx2idf
 	
 	n_item = 0
 	for item in xmlTree.iter(teipath+'biblStruct'):
@@ -74,84 +76,70 @@ def getitem(pdf_path,title_list,dict_output):
 				break
 		for i in range(len(title_list_copy)):
 			if title_list_copy[i][0] == title:
-				title_numb.append((title_list_copy[i][1],n_item))
+				title_numb[title_list_copy[i][1]] = n_item
 				title_list_copy.pop(i)
 				break
 		n_item += 1
 	
+	for ref in tree.iter(teipath+"ref"):
+		if "type" not in ref.attrib or ref.attrib["type"] != "bibr":
+			continue
+		if "target" not in ref.attrib:
+			continue
+		tgt = ref.attrib["target"][2:]
+		if tgt not in idx2idf:
+			idx2idf[tgt] = []
+		txt = re.sub(r'\W','',ref.text)
+		idx2idf[tgt].append(txt)
 	
 	for title,ref in title_list_copy:
 		dict_output['TNF'] += 1
-		title_numb.append((ref,-1))
+		title_numb[ref] = -1
 	
-	return title_numb
+	return title_numb,idx2idf
 
 
-def get_refs():
-
-	dict_output = {"TNF":0,"2SRCNF":0,"2REFNF":0,"2BBLNF":0,"2UNREAD_BBL":0,"GROBID_ERR":0}
+def get_refs(subdirectory=""):
+	
+	path_pdf += subdirectory + "/"
+	df_links = pd.read_csv(path_links,dtype=str)
+	dict_output = {"BNF":0,"TNF":0,"2SRCNF":0,"2REFNF":0,"2BBLNF":0,"2UNREAD_BBL":0,"GROBID_ERR":0}
 	list_pairs = {}
 
-	for f in os.listdir(path_json):
-
-		# open the json file
-		full_path = path_json + "/" + f
-
-		file_name = f[:-5]
-
-		data = json.load(open(full_path))
-		references = data['references']
-
+	df_list = []
+	for f in os.listdir(path_pdf):
+		fname = f[:-4]
 		title_list = []
-
-		# save all the links to other arxiv papers
-		for ref in references:
-			arxivId = ref['arxivId']
-			if arxivId != None:
-				arxivId = re.sub('/','',arxivId)
-				
-				# get infos
-				intents = ref['intent']
-				methodology = 'methodology' in intents
-				result = 'result' in intents
-				background = 'background' in intents
-				influential = ref['isInfluential']
-				
-				# get title
-				title = ref['title'].lower()
+		df_f = df_links[df_links.pdf_from == fname]
+		for _,row in df_f.iterrows():
+				title = row['title'].lower()
 				title = re.sub(r'[^a-z]','',title)
+				arxivId = row['pdf_to']
 				title_list.append((title,arxivId))
 
-				# add pair
-				list_pairs[(file_name,arxivId)] = {
-					'influential':influential,
-					'background':background,
-					'result':result,
-					'methodology':methodology
-					}
-		
 		# check for titles in references
+		refpos = {}
 		if title_list != []:
-			refpos = getitem(path_pdf + file_name + ".pdf",title_list,dict_output)
-			for (ref,pos) in refpos:
-				list_pairs[(file_name,ref)]['grobid_index'] = pos
-				list_pairs[(file_name,ref)]['bibitem'] = None
-		
-	pairs = []
-	for k in list_pairs.keys():
-		v = list_pairs[k]
-		pairs.append([k[0],
-					k[1],
-					v['influential'],
-					v['background'],
-					v['result'],
-					v['methodology'],
-					v['grobid_index']]
-					)
+			refpos,idx2idf = getitem(path_pdf + file_name + ".pdf",title_list,dict_output)
 
-	# save it
-	data = pd.DataFrame(pairs)
-	data.to_csv(STUFF_PATH + '/references_grobid_index.csv',
-				index=False,
-				header=['pdf_from','pdf_to','influential','background','result','methodology','grobid_index'])
+		for _,row in df_f.iterrows():
+			arxivId = row['pdf_to']
+			if arxivId in refpos:
+				row['grobid_index'] = pos
+				if pos in idx2idf:
+					row['tags_2'] = "-".join(idx2idf[pos])
+				else:
+					row['tags_2'] = None
+			else:
+				dict_output["BNF"] += 1
+				row['grobid_index'] = -2
+				row['tags_2'] = None
+
+		
+		df_list.append(df_f)
+	
+
+	data = pd.concat(df_list)	
+	data.to_csv("%s/links_%s.csv"%(GRAPH_PATH,subdirectory),
+				index=False)
 
